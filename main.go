6b5ffbcaeb588011f56732f8b3c5cbc4a19e7351
MirInjamamul <injamul120221@gmail.com>
2023-05-24 18:41:21 +0600

    golang static floder has been added as public

--- main.go
+++ main.go
@@ -15,6 +15,10 @@ func main() {
 	if err := os.MkdirAll("uploads", os.ModePerm); err != nil {
 		log.Fatal("Failed to Create Upload Directory")
 	}
+
+	// Serve static files from the "uploads" directory
+	r.Static("/uploads", "./uploads")
+
 	// Register the route
 
 	view.RegisterRoutes(r)
