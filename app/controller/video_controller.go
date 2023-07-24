25e38514171ddb025e08835565d58ac89cc7c76a
MirInjamamul <injamul120221@gmail.com>
2023-07-24 12:22:51 +0600

    added log for 400

--- app/controller/video_controller.go
+++ app/controller/video_controller.go
@@ -1,7 +1,7 @@
 package controller
 
 import (
-	"fmt"
+	"log"
 	"video-server/app/model"
 
 	"github.com/gin-gonic/gin"
@@ -21,7 +21,7 @@ func (vc *VideoController) UploadVideo(c *gin.Context) {
 	file, err := c.FormFile("video")
 
 	if err != nil {
-		fmt.Sprintf(err.Error())
+		log.Printf(err.Error())
 		c.JSON(400, gin.H{"status": false, "error": "Bad Request - No Video file uploaded"})
 		return
 	}
@@ -29,7 +29,7 @@ func (vc *VideoController) UploadVideo(c *gin.Context) {
 	err = vc.videoModel.SaveVideo(c, file)
 
 	if err != nil {
-		fmt.Sprintf(err.Error())
+		log.Printf(err.Error())
 		c.JSON(500, gin.H{"status": false, "error": "Internal Server Error - Failed to save Video"})
 	} else {
 		c.JSON(200, gin.H{"status": true, "message": "Video File Upload Successfully"})
